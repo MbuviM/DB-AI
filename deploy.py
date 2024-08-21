51ca34562d28926ae62504be7b5a9180677eefe3
MbuviM <mwendembuvi10@gmail.com>
2024-08-21 22:45:58 +0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -31,7 +31,7 @@ def main():
             # Play the audio in Streamlit automatically
             if audio_buffer:
                 try:
-                    st.audio(audio_buffer.getvalue(), format='audio/mp3', start_time=0)
+                    st.audio(audio_buffer.getvalue(), format='audio/mp3', unsafe_allow_html=True, start_time=0)
                 except TypeError:
                     st.write("Sorry, there was an issue playing the audio.")
 
