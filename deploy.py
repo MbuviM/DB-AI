c7155ab7a9e6467fc736e35ba7d1295d4f0e50f6
MbuviM <mwendembuvi10@gmail.com>
2024-08-21 22:42:58 +0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -30,7 +30,7 @@ def main():
 
             # Play the audio in Streamlit automatically
             if audio_buffer:
-                st.audio(audio_buffer.getvalue(), format='audio/mp3', unsafe_allow_html=True, start_time=0, volume=1.0)
+                st.audio(audio_buffer.getvalue(), format='audio/mp3', start_time=0, volume=1.0)
 
         else:
             st.warning("Please enter a question.")
